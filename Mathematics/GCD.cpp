38e2c8649a6dd98a7a2f488c8d164fa7890dc621
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-03 10:05:13 +0530

    GCD

---
+++ Mathematics/GCD.cpp
@@ -0,0 +1,28 @@
+//Naive approach
+
+int gcd(int a, int b)
+{
+    int min = min(a, b);
+    int maxDivisor = INT_MIN;
+    for (int i = 1; i <= min; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            if (maxDivisor < i)
+            {
+                maxDivisor = i;
+            }
+        }
+    }
+    return maxDivisor;
+}
+
+// Optimised Approach(Euclidian theorem)
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
