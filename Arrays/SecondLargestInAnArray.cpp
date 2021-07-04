ac39df33baae0cdf0db3c1ce80c53d218efbb998
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-04 21:42:07 +0530

    Second Largest In An Array

---
+++ Arrays/SecondLargestInAnArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int LargestInAnArray(vector<int> &v, int n)
+{
+    int n = v.size();
+    int maxIndex = 0;
+    int secMaxIndex = 0;
+    for (int i = 1; i < n; i++) // find the maxIndex
+    {
+        if (v[i] > v[maxIndex])
+        {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+int secondLargest(vector<int> &v, int n)
+{
+
+    int largest = LargestInAnArray(v, n);
+    int res = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] != v[largest])
+        {
+            if (res == -1)
+            {
+                res = i;
+            }
+            else if (v[i] > v[res])
+            {
+                res = i;
+            }
+        }
+    }
+    return res;
+}
