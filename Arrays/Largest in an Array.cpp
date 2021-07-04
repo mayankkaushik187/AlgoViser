d36d02f0c21c5786575869b628f793273b38e0ca
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-04 21:13:55 +0530

    Largest In an Array

---
+++ Arrays/Largest in an Array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int largestInAnArray(vector<int> &v)
+{
+    int maxIndex = 0;
+    int n = v.size();
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] > v[maxIndex])
+        {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
