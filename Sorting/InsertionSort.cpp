4513872b6dc5b86cf5326d9d6e6536f7eec2aa6f
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-17 06:09:19 +0530

    Insertion Sort

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
