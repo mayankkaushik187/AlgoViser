d3585dc5c2b4b894caa72e4d0f9127870bd18394
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-17 05:32:56 +0530

    Bubble Sort Optimised

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    bool swapped = false;
+    for (int i = 0; i < n; i++)
+    {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+}
