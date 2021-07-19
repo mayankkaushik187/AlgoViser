0b6a01d67d53872beafea03a40c23d8ae2384071
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-20 04:03:05 +0530

    Transpose of a Matrix

---
+++ Matrix/Transpose.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<vector<int>> v)
+{
+    int n = v.size();
+    int m = v[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < m; j++)
+        {
+            swap(v[i][j], v[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<vector<int>> v = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    print(v);
+    return 0;
+}
