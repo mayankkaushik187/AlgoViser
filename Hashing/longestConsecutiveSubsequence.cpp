26f6c9a64a3593f91c34b0e2cd33cdba6ad9b40b
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-22 03:51:54 +0530

    Longest Consecutive Subsequence

--- Hashing/longestConsecutiveSubsequence.cpp
+++ Hashing/longestConsecutiveSubsequence.cpp
@@ -4,23 +4,43 @@ using namespace std;
 
 int longestConsecSeq(int arr[], int n)
 {
+    //naive method
+    // sort(arr, arr + n);
+    // int len = 1;
+    // int maxLen = 1;
+    // for (int i = 1; i < n; i++)
+    // {
+    //     if (arr[i - 1] + 1 == arr[i])
+    //     {
+    //         len++;
+    //     }
+    //     else if (arr[i] != arr[i - 1])
+    //     {
+    //         maxLen = max(len, maxLen);
+    //         len = 1;
+    //     }
+    // }
+    // return max(maxLen, len);
 
-    sort(arr, arr + n);
+    unordered_set<int> s;
     int len = 1;
-    int maxLen = 1;
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (arr[i - 1] + 1 == arr[i])
+        s.insert(arr[i]);
+    }
+    for (auto x : s)
+    {
+        if (s.find(x - 1) == s.end())
         {
-            len++;
-        }
-        else if (arr[i] != arr[i - 1])
-        {
-            maxLen = max(len, maxLen);
-            len = 1;
+            int curr = 1;
+            while (s.find(x + curr) != s.end())
+            {
+                curr++;
+            }
+            len = max(len, curr);
         }
     }
-    return max(maxLen, len);
+    return len;
 }
 
 int main()
