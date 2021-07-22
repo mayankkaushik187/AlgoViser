e548865b19ed252da866ab9ef6d22dbe599a5ca0
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-23 04:49:42 +0530

    Check if palindrome

---
+++ Strings/checkIfPalin.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string str)
+{
+    int start = 0;
+    int end = str.length() - 1;
+    while (start <= end)
+    {
+        if (str[start] != str[end])
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+
+int main()
+{
+    string str = "rafagasdgsdgadgadgadgqweqwe";
+    cout << check(str);
+    return 0;
+}
