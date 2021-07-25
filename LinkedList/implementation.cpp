f22689c1a18fd1e0fa3831e1896c218cc9b3b1b4
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-25 11:14:44 +0530

    Implementation of a Linked List

---
+++ LinkedList/implementation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    return 0;
+}
