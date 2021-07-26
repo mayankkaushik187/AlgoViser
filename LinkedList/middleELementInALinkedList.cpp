f0236dd2d833efd5822ddd105c1ead7446df1513
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-26 16:18:15 +0530

    Middle element in a LL

---
+++ LinkedList/middleELementInALinkedList.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+void printMiddle(Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    Node *fast = head;
+    Node *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    cout << slow->data;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    printlist(head);
+    cout << "Middle of Linked List: ";
+    printMiddle(head);
+    return 0;
+}
