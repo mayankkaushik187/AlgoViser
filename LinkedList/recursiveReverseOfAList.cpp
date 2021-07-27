b5f2797b0ffb9960e2e43789466a7066a372b51a
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-27 13:27:31 +0530

    Recursive reverse of a tail and iterative approach as well

---
+++ LinkedList/recursiveReverseOfAList.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node *reverseList(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    Node *Revhead = reverseList(head->next);
+    Node *tail = head->next;
+    tail->next = head;
+    head->next = NULL;
+    return Revhead;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    printlist(head);
+    cout << "Reversed Linked List: ";
+    head = reverseList(head);
+    printlist(head);
+    return 0;
+}
---
+++ LinkedList/reverseAList.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node *reverseList(Node *head)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *next = curr->next;
+
+    if (head == NULL)
+    {
+        return head;
+    }
+    if (head->next == NULL)
+    {
+        return head;
+    }
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    printlist(head);
+    cout << "Reversed Linked List: ";
+    head = reverseList(head);
+    printlist(head);
+    return 0;
+}
