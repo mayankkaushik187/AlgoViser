7da30385a0093e7deab8d4c6b296bb49160015e0
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-08-25 12:50:27 +0530

    Replace nodes with sum of greater nodes

---
+++ BST/replace_nodes_with_sum_of_greater_elements.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+static int sum = 0; // bad practice  should not use global vars and should be passed as an argument.
+
+void replaceWithLargerNodesSum(BinaryTreeNode<int> *root)
+{
+    // Write your code here
+    if (!root)
+        return;
+
+    replaceWithLargerNodesSum(root->right);
+    //data retain karo in a temp var and then chnge the root data and then chnge the sum by adding temp into it.
+    int temp = root->data;
+    root->data = sum + root->data;
+    sum += temp;
+
+    replaceWithLargerNodesSum(root->left);
+}
