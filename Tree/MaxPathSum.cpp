6a394c1ff4d5426d98094537d46ac0647988361e
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-08-27 04:50:01 +0530

    Maximum Path Sum

---
+++ Tree/MaxPathSum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+int help(Node *root, int &maxi)
+{
+    if (!root)
+        return 0;
+
+    int lans = max(0, help(root->left, maxi));
+    int rans = max(0, help(root->right, maxi));
+
+    maxi = max(maxi, root->data + lans + rans);
+
+    return root->data + max(lans, rans);
+}
+
+int findMaxSum(Node *root)
+{
+    // Your code goes here
+    if (!root)
+        return 0;
+    int maxi = INT_MIN;
+    help(root, maxi);
+
+    return maxi;
+}
