dc3bcadc427b7c7d5f0bf9b08efec9ba3179e744
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-09-08 12:55:01 +0530

    Jenga problem

--- Dynamic_Programming/Coin_tower_jenga_problem.cpp
+++ Dynamic_Programming/Coin_tower_jenga_problem.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+QUESTION STATEMENT
+Whis and Beerus are playing a new game today. 
+They form a tower of N coins and make a move in alternate turns. 
+Beerus plays first. In one step, 
+the player can remove either 1, X, or Y coins from the tower. 
+The person to make the last move wins the game. 
+Can you find out who wins the game?
+*/
 string findWinner(int n, int x, int y)
 {
     // Write your code here .
