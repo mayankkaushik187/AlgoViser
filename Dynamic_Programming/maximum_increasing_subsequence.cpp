b56d849616790d0e96538bb60f55e67d28cf2e9c
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-09-09 22:39:21 +0530

    Maximum increasing subsequence sum

---
+++ Dynamic_Programming/maximum_increasing_subsequence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int MSIS(int arr[], int n)
+{
+    vector<int> dp(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = arr[i];
+        for (int j = 0; j < i; i++)
+        {
+            if (arr[j] < arr[i])
+            {
+                dp[i] = max(dp[i], arr[i] + dp[j]);
+            }
+        }
+    }
+    int res = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        res = max(res, dp[i]);
+    }
+
+    return res;
+}
+
+int main()
+{
+
+    int n = 3;
+
+    int arr[] = {5, 10, 20};
+
+    cout << MSIS(arr, n);
+
+    return 0;
+}
