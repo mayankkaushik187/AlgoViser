20d66d63e7b635c8c0bd84ff066a80501fd97148
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-09-11 20:18:22 +0530

    Subset sum Problem DP Solution

--- Dynamic_Programming/Subset_sum_problem.cpp
+++ Dynamic_Programming/Subset_sum_problem.cpp
@@ -19,12 +19,15 @@ int subset_sum_dp(int arr[], int n, int sum)
     for (int i = 1; i <= sum; i++)
     {
         dp[0][i] = 0;
+    }
+    for (int i = 1; i <= n; i++)
+    {
         dp[i][0] = 1;
     }
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j <= sum; j++)
+        for (int j = 1; j <= sum; j++)
         {
             if (j >= arr[i - 1])
                 dp[i][j] = dp[i - 1][j] + dp[i - 1][j - arr[i - 1]];
@@ -40,6 +43,6 @@ int main()
 {
     int n = 4;
     int arr[] = {10, 20, 15, 2};
-    cout << "Recursion = " << subset_sum_recur(arr, n, 2) << endl;
-    cout << "Dp Solution = " << subset_sum_dp(arr, n, 2) << endl;
+    cout << "Recursion = " << subset_sum_recur(arr, n, 25) << endl;
+    cout << "Dp Solution = " << subset_sum_dp(arr, n, 25) << endl;
 }
