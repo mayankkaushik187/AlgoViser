8567813480e8f7e9ddd969d75a93837f79a1f85d
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-09-13 23:56:36 +0530

    Longest Increasing Subsequence

--- Dynamic_Programming/Longest_Increasing_Subsequence.cpp
+++ Dynamic_Programming/Longest_Increasing_Subsequence.cpp
@@ -28,6 +28,48 @@ int longestIncreasingSubsequence(int *input, int n)
     return res;
 }
 
+int ceilArr(int tail[], int l, int r, int x)
+{
+    while (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+        if (tail[m] >= x)
+        {
+            r = m;
+        }
+        else
+        {
+            l = m + 1;
+        }
+    }
+
+    return r;
+}
+
+int longestSubsequence_binarySearch(int n, int arr[])
+{
+    int len = 1;
+
+    int tail[n];
+    tail[0] = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > tail[len - 1])
+        {
+            tail[len] = arr[i];
+            len++;
+        }
+        else
+        {
+            int c = ceilArr(tail, 0, len - 1, arr[i]);
+            tail[c] = arr[i];
+        }
+    }
+    return len;
+}
+
 int main()
 {
     int n;
