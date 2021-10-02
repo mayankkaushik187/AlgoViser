c4edf9c9b5ab802326462140e8fb9526a55072a6
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-10-03 03:32:56 +0530

    Count occurrence of anagrams in a txt

---
+++ Strings/Count Occurences of Anagrams.cpp
@@ -0,0 +1,37 @@
+bool compare(char arr1[], char arr2[])
+{
+    for (int i = 0; i < 256; i++)
+    {
+        if (arr1[i] != arr2[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+int search(string pat, string txt)
+{
+    char patC[256] = {0}, txtC[256] = {0};
+    int n = pat.size(), m = txt.size();
+    for (int i = 0; i < n; i++)
+    {
+        patC[pat[i]]++;
+        txtC[txt[i]]++;
+    }
+    int count = 0;
+    for (int i = n; i < m; i++)
+    {
+        if (compare(patC, txtC))
+        {
+            count++;
+        }
+        txtC[txt[i]]++;
+        txtC[txt[i - n]]--;
+    }
+    if (compare(patC, txtC))
+    {
+        count++;
+    }
+    return count;
+}
