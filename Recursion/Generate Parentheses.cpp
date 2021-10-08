3e4fbabc771c07a34882ab29119e7cd7ac377578
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-10-09 02:06:28 +0530

    generate paentheses

---
+++ Recursion/Generate Parentheses.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void helper(int open, int close, vector<string> &res, stack<char> s, int n)
+{
+    if (open == close and close == n)
+    {
+        string str = "";
+        while (!s.empty())
+        {
+            str += s.top();
+            s.pop();
+        }
+        reverse(str.begin(), str.end());
+        res.push_back(str);
+    }
+    if (open < n)
+    {
+        s.push('(');
+        helper(open + 1, close, res, s, n);
+        s.pop();
+    }
+    if (close < open)
+    {
+        s.push(')');
+        helper(open, close + 1, res, s, n);
+        s.pop();
+    }
+}
+vector<string> generateParenthesis(int n)
+{
+    vector<string> res;
+    stack<char> s;
+    helper(0, 0, res, s, n);
+    return res;
+}
