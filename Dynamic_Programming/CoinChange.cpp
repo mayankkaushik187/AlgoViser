8270067c466081e8d7c3eb1fcc11267e2ef5183e
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-10-14 00:53:52 +0530

    Create CoinChange.cpp

---
+++ Dynamic_Programming/CoinChange.cpp
@@ -0,0 +1,19 @@
+//Leetcode 322 
+
+int coinChange(vector < int > & coins, int W) {
+  int n = coins.size();
+  vector < int > dp(W + 1, W + 1);
+  if (n == 1) {
+
+  }
+  dp[0] = 0;
+
+  for (unsigned int i = 1; i <= W; i++) {
+    for (unsigned int j = 0; j < n; j++) {
+      if (coins[j] <= i) {
+        dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+  }
+  return dp[W] > W ? -1 : dp[W];
+}
