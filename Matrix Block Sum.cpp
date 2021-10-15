0d3416a8546a5507c047cf81956abb42b0b8f271
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-10-15 13:45:15 +0530

    Create Matrix Block Sum.cpp

---
+++ Matrix Block Sum.cpp
@@ -0,0 +1,31 @@
+vector < vector < int >> matrixBlockSum(vector < vector < int >> & mat, int k) {
+  int n = mat.size();
+  int m = mat[0].size();
+
+  int sum[n + 1][m + 1];
+  memset(sum, 0, sizeof sum);
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      sum[i][j] = sum[i][j - 1] + sum[i - 1][j] + mat[i - 1][j - 1] - sum[i - 1][j - 1];
+    }
+  }
+
+  vector < vector < int >> res(n, vector < int > (m));
+  for (int r = 0; r < n; r++) {
+    for (int c = 0; c < m; c++) {
+      int r1 = max(0, r - k);
+      int r2 = min(n - 1, r + k);
+      int c1 = max(0, c - k);
+      int c2 = min(m - 1, c + k);
+      r1++;
+      r2++;
+      c1++;
+      c2++;
+
+      res[r][c] = sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
+    }
+  }
+
+  return res;
+}
