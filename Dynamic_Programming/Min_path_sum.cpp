0a9b9066e1a3fb9c7cc969e3346c0b4b5b571543
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-10-16 20:50:25 +0530

    Create Min_path_sum.cpp

---
+++ Dynamic_Programming/Min_path_sum.cpp
@@ -0,0 +1,22 @@
+//Leetcode 64
+
+int minPathSum(vector < vector < int >> & grid) {
+    int n = grid[0].size();
+    int m = grid.size();
+    vector < vector < int >> dp(m, vector < int > (n));
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < n; i++) {
+        dp[0][i] = grid[0][i] + dp[0][i - 1];
+    }
+    for (int i = 1; i < m; i++) {
+        dp[i][0] = grid[i][0] + dp[i - 1][0];
+    }
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
