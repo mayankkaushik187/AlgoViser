3ee55761e0c5dce980c74ae983f2772f616cf88f
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-11-03 23:11:53 +0530

    Create Substring with Concatenation of all words.cpp

---
+++ Strings/Substring with Concatenation of all words.cpp
@@ -0,0 +1,24 @@
+//Leetcode 30
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string,int> count;
+        for(auto x : words)count[x]++;
+        vector<int> res;
+        int len = words.size() * words[0].size();
+        if(len > s.size())return res;
+        for(int i = 0 ; i <= s.size() - len ; i++){
+            unordered_map<string,int> seen;
+            int j;
+            for(j = 0 ; j < len ; j += words[0].size()){
+                string str = s.substr(i + j, words[0].size());
+                if(count.find(str)!=count.end()){
+                    seen[str]++;
+                    if(seen[str] > count[str])break;
+                }else break;
+            }
+            if(j == len)res.push_back(i);
+        }
+        return res;
+    }
+};
