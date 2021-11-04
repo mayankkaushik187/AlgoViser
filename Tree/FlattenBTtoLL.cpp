f1c5a191dbe4bffa74bc9045986a30c7399993b2
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-11-04 22:12:04 +0530

    Create FlattenBTtoLL.cpp

---
+++ Tree/FlattenBTtoLL.cpp
@@ -0,0 +1,31 @@
+//Leetcode 114
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root)return;
+        auto curr = root;
+        while(curr){
+            if(curr->left){
+                auto prev = curr->left;
+                while(prev->right){
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
