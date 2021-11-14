7bd196da6760f394150e92247ce60e3fe42cfe82
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-11-14 09:47:52 +0530

    Update reverseInGroupOfK.cpp

--- LinkedList/reverseInGroupOfK.cpp
+++ LinkedList/reverseInGroupOfK.cpp
@@ -1,6 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+//Leetcode Soln
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head or k == 1)return head;
+        ListNode * dummy = new ListNode(0);
+        dummy->next = head;
+        auto prev = dummy, curr = dummy, nex = dummy;
+        //count the number of nodes
+        int count = 0;
+        while(curr->next != NULL){
+            curr = curr->next;
+            count++;
+        }
+        while(count >= k){
+            curr = prev->next;
+            nex = curr->next;
+            for(int i = 0 ; i < k - 1 ; i++){
+                curr->next = nex->next;
+                nex->next = prev->next;
+                prev->next = nex;
+                nex = curr->next;
+            }
+            count -= k;
+            prev = curr;
+        }
+        return dummy->next;
+    }
+};
 
+//_______________________________________________________________
 struct Node
 {
     int data;
