458846dbf78741c2834fa1674448a5c9f856a111
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-11-21 16:51:24 +0530

    Update Course_Schedule.cpp

--- Graphs/Course_Schedule.cpp
+++ Graphs/Course_Schedule.cpp
@@ -29,3 +29,49 @@ public:
         return true;
     }
 };
+
+
+//Topological Sort method
+class Solution {
+private:
+    queue<int> getZeroInDegreeNode(vector<int>& indegree) {
+
+        int n = indegree.size();
+        queue<int> q;
+        for (int i = 0; i < n ; i++) {
+            if (indegree[i] == 0)q.push(i);
+        }
+        return q;
+    }
+    void bfs(queue<int>&q, vector<int>&indegree, int &numCourses, vector<int> adj[]) {
+        //the idea is that we need to complete all the courses successfully so the
+        //the number of time this while loop runs should be equal to the number courses
+        while (!q.empty()) {
+            auto fr = q.front();
+            q.pop();
+            numCourses--;
+            for (auto &edges : adj[fr]) {
+                if (--indegree[edges] == 0) {
+                    q.push(edges);
+                }
+            }
+        }
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        
+        vector<int> indegree(numCourses, 0);
+
+        for (auto&edges : prerequisites) {
+            indegree[edges[0]]++;
+            adj[edges[1]].push_back(edges[0]);
+        }
+
+        queue<int> q = getZeroInDegreeNode(indegree);
+
+        bfs(q, indegree, numCourses, adj);
+
+        return numCourses == 0;
+    }
+};
