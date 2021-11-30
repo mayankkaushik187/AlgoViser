bd04cea06adf683e1f11c2b450efb3ddf64bcf50
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-11-30 21:19:50 +0530

    Create Permutations.cpp

---
+++ CSES/Permutations.cpp
@@ -0,0 +1,26 @@
+void solve() {
+	int n;
+	cin >> n;
+	if (n == 3 or n == 2) {
+		cout << "NO SOLUTION" << nline;
+		return;
+	}
+	for (int i = 2; i <= n ; i += 2) {
+		cout << i << " ";
+	}
+	for (int i = 1 ; i <= n ; i += 2) {
+		cout << i << " ";
+	}
+
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	solve();
+	return 0;
+}
