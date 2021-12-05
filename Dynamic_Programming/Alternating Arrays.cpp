ac77f15ca316094324599dc9f345cfb4520234ae
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-12-05 17:04:09 +0530

    Create Alternating Arrays.cpp

---
+++ Dynamic_Programming/Alternating Arrays.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nline "\n"
+#define int long long int
+#define all(x) x.begin(),x.end()
+#define pb push_back
+vector<bool> sieve(int n){vector<bool> si(n + 1,true);si[0] = false;si[1] = false;for(int i = 2 ; i <= sqrt(n) ; i++){if(si[i]){for(int j = i*i ; j <= n ; j += i){si[j] = false;}}}return si;}
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void solve(){
+	int n;
+	cin>>n;
+	vector<int> arr(n);
+	for(int i =0 ; i < n ; i++){
+		cin>>arr[i];
+	}
+
+	vector<int> dp(n,1);
+	for(int i = n - 2; i >= 0 ; i--){
+		if(arr[i] > 0 and arr[i+1] < 0 or (arr[i+1] > 0 and arr[i] < 0)){
+			dp[i] = max(dp[i],dp[i+1] + 1);
+		}
+	}
+	for(int i = 0 ; i < dp.size() ; i++){
+		cout<<dp[i]<<" ";
+	}
+	cout<<nline;
+} 
+
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r" , stdin);
+	freopen("output1.txt" ,"w" , stdout);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
