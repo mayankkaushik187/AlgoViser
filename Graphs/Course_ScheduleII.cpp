6ea1f9c309f7fbebf9cd8eaa427a2d0af728bf37
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-12-23 12:45:29 +0530

    Update Course_ScheduleII.cpp

--- Graphs/Course_ScheduleII.cpp
+++ Graphs/Course_ScheduleII.cpp
@@ -1,52 +1,47 @@
 class Solution {
 private:
-    void fillTheList(vector<int> adj[], vector<int>&indegree, int n, vector<vector<int>>&prerequisites) {
-
-        for (auto &edge : prerequisites) {
-            adj[edge[1]].push_back(edge[0]);
-            indegree[edge[0]]++;
+    void fillTheList(vector<int> adj[],int numCourses,vector<vector<int>>&prerequisites,vector<int>&indegree){
+        for(int i = 0 ; i < prerequisites.size() ; i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
         }
     }
-    void pushNodeWithZeroIndegree(queue<int> &q, vector<int>&indegree, int n, vector<int>&result) {
-        for (int i = 0 ; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-                result.push_back(i);
+    void initTheQueue(queue<int>& Q,vector<int>&indegree){
+        for(int node = 0 ; node < indegree.size() ; node++){
+            if(indegree[node] == 0){
+                Q.push(node);
             }
         }
     }
-    void bfs(vector<int>&temp, vector<int>adj[], queue<int>&q, vector<int>&indegree, int &n) {
-        while (!q.empty()) {
-            auto front = q.front();
-            q.pop();
-            n--;
-            //temp.push_back(front);
-            for (auto &edge : adj[front]) {
-                if (--indegree[edge] == 0) {
-                    q.push(edge);
-                    temp.push_back(edge);
+    
+    void bfs(vector<int>&result,queue<int>& Q, vector<int>&indegree,vector<int> adj[],int &numCourses){
+        while(!Q.empty()){
+            int front = Q.front();
+            Q.pop();
+            result.push_back(front);
+            numCourses--;
+            for(auto &nbr : adj[front]){
+                if(--indegree[nbr] == 0){
+                    Q.push(nbr);
                 }
             }
         }
     }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> result;
-        int n = numCourses;
-        vector<int> indegree(n, 0);
-
         vector<int> adj[numCourses];
-
-        fillTheList(adj, indegree, n, prerequisites);
-
-        queue<int> q;
-
-        pushNodeWithZeroIndegree(q, indegree, n, result);
-
-        bfs(result, adj, q, indegree, n);
-
-        if(n == 0)return result;
+        vector<int> indegree(numCourses,0);
+        queue<int> Q;
+        vector<int> result;
+        
+        fillTheList(adj,numCourses,prerequisites,indegree);
+        
+        initTheQueue(Q,indegree);
+        
+        bfs(result,Q,indegree,adj,numCourses);
+        
+        if(numCourses == 0)return result;
+        
         return {};
     }
 };
-
