575a0ed8fdda8c3bf0cceebb29a6d2b90d6ff0b7
Mayank Kaushik <mayankkaushik187@gmail.com>
2022-01-15 18:58:18 +0530

    Update All_nodes_at_k_dist_from_target.cpp

--- BST/All_nodes_at_k_dist_from_target.cpp
+++ BST/All_nodes_at_k_dist_from_target.cpp
@@ -1,60 +1,69 @@
 //Leetcode 863
-void mark_parent(TreeNode * root, unordered_map < TreeNode * , TreeNode * > & parent_map) {
-  queue < TreeNode * > q;
-  q.push(root);
-  parent_map[root] = NULL;
-  while (!q.empty()) {
-    int k = q.size();
-    for (int i = 0; i < k; i++) {
-      auto fr = q.front();
-      if (fr -> left) {
-        parent_map[fr -> left] = fr;
-        q.push(fr -> left);
-      }
-      if (fr -> right) {
-        parent_map[fr -> right] = fr;
-        q.push(fr -> right);
-      }
-      q.pop();
+
+class Solution {
+private:
+    void mark_parent(unordered_map<TreeNode* , TreeNode*> &parent_map,TreeNode* root){
+        queue<TreeNode*> Q;
+        Q.push(root);
+        parent_map[root] = NULL;
+        while(!Q.empty()){
+            int size = Q.size();
+            while(size--){
+                auto curr = Q.front();
+                Q.pop();
+                if(curr -> left){
+                    parent_map[curr->left] = curr;
+                    Q.push(curr -> left);
+                }
+                if(curr -> right){
+                    parent_map[curr->right] = curr;
+                    Q.push(curr -> right);
+                }
+            }
+        }
     }
-  }
-}
-vector < int > distanceK(TreeNode * root, TreeNode * target, int k) {
-  unordered_map < TreeNode * , TreeNode * > parent_map;
-  mark_parent(root, parent_map);
-
-  unordered_map < TreeNode * , bool > visited;
-  queue < TreeNode * > q;
-  q.push(target);
-  visited[target] = true;
-  int level = 0;
-  while (!q.empty()) {
-    int size = q.size();
-    if (level == k) break;
-    level++;
-    for (int i = 0; i < size; i++) {
-      auto curr = q.front();
-      q.pop();
-      if (curr -> left and!visited[curr -> left]) {
-        q.push(curr -> left);
-        visited[curr -> left] = true;
-      }
-      if (curr -> right and!visited[curr -> right]) {
-        q.push(curr -> right);
-        visited[curr -> right] = true;
-      }
-      if (parent_map[curr] and!visited[parent_map[curr]]) {
-        q.push(parent_map[curr]);
-        visited[parent_map[curr]] = true;
-      }
+    void runBFS(unordered_map<TreeNode* , bool>&visited,TreeNode* root,unordered_map<TreeNode*, TreeNode*> &parent_map,TreeNode* target,int k,queue<TreeNode* > &Q){
+        Q.push(target);
+        visited[target] = true;
+        int level = 0;
+        while(!Q.empty()){
+            int size = Q.size();
+            if(level == k)break;
+            while(size--){
+                auto curr = Q.front();
+                Q.pop();
+                if(curr -> left and !visited[curr -> left]){
+                    Q.push(curr -> left);
+                    visited[curr -> left] = true;
+                }
+                if(curr -> right and !visited[curr -> right]){
+                    Q.push(curr -> right);
+                    visited[curr -> right] = true;
+                }
+                if(parent_map[curr] and !visited[parent_map[curr]]){
+                    Q.push(parent_map[curr]);
+                    visited[parent_map[curr]] = true;
+                }
+            }
+            level++;
+        }
     }
-  }
-  vector < int > res;
-  while (!q.empty()) {
-    res.push_back(q.front() -> val);
-    q.pop();
-  }
-
-  return res;
-
-}
+    void fillVector(queue<TreeNode*> &Q,vector<int>&res){
+        while(!Q.empty()){
+            auto front = Q.front();
+            Q.pop();
+            res.push_back(front -> val);
+        }
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode* , TreeNode* > parent_map;
+        mark_parent(parent_map,root);
+        unordered_map<TreeNode* , bool> visited;
+        queue<TreeNode*> Q;
+        runBFS(visited,root,parent_map,target,k,Q);
+        vector<int> res;
+        fillVector(Q,res);
+        return res;
+    }
+};
