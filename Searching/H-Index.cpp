21c111ebb6941eed4e7adf9e40f88e5f69075f4c
Mayank Kaushik <mayankkaushik187@gmail.com>
2022-01-15 11:59:35 +0530

    Update H-Index.cpp

--- Searching/H-Index.cpp
+++ Searching/H-Index.cpp
@@ -30,3 +30,28 @@ int hIndex(vector<int> &citations)
     sort(citations.begin(), citations.end());
     return hIndexHelper(citations);
 }
+
+//other way
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        unordered_map<int,int> mp;
+        int n = citations.size();
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(citations[i] >= n)mp[n]++;
+            else mp[citations[i]]++;
+        }
+        
+        int paper = 0;
+        for(int i = n ; i >= 0 ; i--){
+            paper += mp[i];
+            if(paper >= i){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+//O(n) time 
+//O(n) space
