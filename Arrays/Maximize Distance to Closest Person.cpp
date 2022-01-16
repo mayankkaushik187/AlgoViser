f3c0232f7711b59d463024b0469c142fe0a88c98
Mayank Kaushik <mayankkaushik187@gmail.com>
2022-01-16 11:35:29 +0530

    Update Maximize Distance to Closest Person.cpp

--- Arrays/Maximize Distance to Closest Person.cpp
+++ Arrays/Maximize Distance to Closest Person.cpp
@@ -29,3 +29,21 @@ public:
         return maxm;
     }
 };
+//O(1) space
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int res = INT_MIN;
+        int last = -1;
+        int n = seats.size();
+        for(int i = 0 ; i < n ; i++){
+            if(seats[i]){
+                res = last < 0 ? i : max(res,(i - last) / 2);
+                last = i;
+            }
+        }
+        
+        res = max(res, n - 1 - last);
+        return res;
+    }
+};
