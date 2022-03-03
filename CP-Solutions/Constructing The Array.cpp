7c273396c79d75347918f6c6fab17540b4c14e62
Mayank Kaushik <mayankkaushik187@gmail.com>
2022-03-03 22:16:23 +0530

    Create Constructing The Array.cpp

---
+++ CP-Solutions/Constructing The Array.cpp
@@ -0,0 +1,177 @@
+// ▒▒▒▒▒▒▒▒▒▒▒▄▄▄▄░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒▒▒▒▒▒▄██████▒▒▒▒▒▄▄▄█▄▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒▒▒▒▄██▀░░▀██▄▒▒▒▒████████▄▒▒▒▒▒▒
+// ▒▒▒▒▒▒███░░░░░░██▒▒▒▒▒▒█▀▀▀▀▀██▄▄▒▒▒
+// ▒▒▒▒▒▄██▌░░░░░░░██▒▒▒▒▐▌▒▒▒▒▒▒▒▒▀█▄▒
+// ▒▒▒▒▒███░░▐█░█▌░██▒▒▒▒█▌▒▒▒▒▒▒▒▒▒▒▀▌
+// ▒▒▒▒████░▐█▌░▐█▌██▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▐████░▐░░░░░▌██▒▒▒█▌▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒████░░░▄█░░░██▒▒▐█▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒████░░░██░░██▌▒▒█▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒████▌░▐█░░███▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒▐████░░▌░███▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒▒▒████░░░███▒▒▒▒█▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▒▒██████▌░████▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒▐████████████▒▒███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ▒█████████████▄████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ██████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ██████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// █████████████████▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// █████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+// ████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#include <bits/stdc++.h>
+using namespace std;
+#define nline "\n"
+#define int long long int
+#define all(x) x.begin(),x.end()
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+// using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define rep(x,a,b) for(int (x) = (a) ; (x) < (b) ; (x)++)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define vi vector<int>
+#define no "NO"
+#define yes "YES"
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+//Print Vector
+template<typename T_vector>
+void print_vector(const T_vector &v, bool add_one = false, int start = -1, int end = -1) {
+	if (start < 0) start = 0;
+	if (end < 0) end = (int)(v.size());
+
+	for (int i = start; i < end; i++)
+		cout << v[i] + (add_one ? 1 : 0) << (i < end - 1 ? ' ' : '\n');
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(int a, int b, int*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); int x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+int mminv(int a, int b) {int arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+int mminvprime(int a, int b) {return expo(a, b - 2, b);}
+bool revsort(int a, int b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+int combination(int n, int r, int m, int *fact, int *ifact) {int val1 = fact[n]; int val2 = ifact[n - r]; int val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int mod_div(int a, int b, int m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+int phin(int n) {int number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (int i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+vector<bool> isprime(int n) { vector<bool> sieve(n + 1, true); for (int i = 2 ; i * i <= n ; i++) { if (sieve[i]) { for (int j = 2 * i ; j <= n ; j += i) { sieve[j] = false; } } } return sieve; }
+/*--------------------------------------------------------------------------------------------------------------------------*/
+struct cmp {
+	bool operator() (const pair<int, int> &a, const pair<int, int> &b) const {
+		int lena = a.ss - a.ff + 1;
+		int lenb = b.ss - b.ff + 1;
+		if (lena == lenb) return a.ff < b.ff;
+		return lena > lenb;
+	}
+};
+
+void solve() {
+	int n;
+	cin >> n;
+	set<pair<int, int>, cmp> st;
+	vector<int> res(n + 1, 0);
+	st.insert({1, n});
+	int id = 1;
+	while (sz(st)) {
+		auto front = *st.begin();
+		st.erase(st.begin());
+		int len = front.ss - front.ff + 1;
+		if (len & 1) {
+			int mid = (front.ff + front.ss) / 2;
+			res[mid] = id++;
+			if (mid - front.ff >= 1)st.insert({front.ff, mid - 1});
+			if (front.ss - mid >= 1)st.insert({mid + 1, front.ss});
+		} else {
+			int mid = (front.ff + front.ss - 1) / 2;
+			res[mid] = id++;
+			if (mid - front.ff >= 1)st.insert({front.ff, mid - 1});
+			if (front.ss - mid >= 1)st.insert({mid + 1, front.ss});
+		}
+	}
+	for (int i = 1 ; i <= n ; i++) {
+		cout << res[i] << " ";
+	}
+	cout << nline;
+}
+
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r" , stdin);
+	freopen("output.txt" , "w" , stdout);
+	freopen("Error1.txt", "w", stderr);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// cout << fixed << setprecision(18);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+		// cout << '\n';
+	}
+	return 0;
+}
+//Bhai please thoda dhang se soch.
+/*
+Some things to remember when you're stuck:
+1. Check for edge cases.
+2. Stay Calm.
+3. Don't be .stupid (search for silly mistakes).
+4. Read problem again. Approach solution from different point of view.
+5. In case of modulo, check for negative result (add MOD).
+
+Some common C++ pit falls:
+1. Don't use inbuilt ceil.
+2. Never take inputs as double unless it is necessary.
+3. Don't pass INT in accumulate.
+*/
